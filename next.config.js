8eab81b55fdeec64572dfe6e607a1b6cec7e1ae0
Prathamesh Pawar <79742694+Prathamesh111-netizen@users.noreply.github.com>
2023-10-31 00:14:37 +0530

    next config update

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
-  experimental: {
-    serverActions: true,
-  },
+  // output: 'export',
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "images.unsplash.com",
+  //     },
+  //   ],
+  // },
+  // experimental: {
+  //   serverActions: true,
+  // },
 };
 
 module.exports = nextConfig;
