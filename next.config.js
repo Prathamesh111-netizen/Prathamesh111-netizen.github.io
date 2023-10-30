ebee03f46892b30dca6090dc3b93518297a19be8
Prathamesh Pawar <79742694+Prathamesh111-netizen@users.noreply.github.com>
2023-10-31 00:16:03 +0530

    next config update

--- next.config.js
+++ next.config.js
@@ -9,9 +9,9 @@ const nextConfig = {
   //     },
   //   ],
   // },
-  // experimental: {
-  //   serverActions: true,
-  // },
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
